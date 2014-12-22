1ed4df99587be19d5d8d08059c5f140a6928fd09
freekv <freek.verstringe@oma.be>
2014-12-22 23:50:43 +0100

    objectify

--- examples/js/gui.js
+++ examples/js/gui.js
@@ -1,78 +1,82 @@
+//Uses globals: objectList from webgl.js
+
 gui = function() {
+    this.datasetGUIObject;
+    this.controlpanel = document.getElementById("controlpanel");
+    this.local_controlpanel = document.getElementById("local_controlpanel");
+
 };
 gui.prototype.datasetGUIObject = {};
-gui.prototype.reinsertCombo = function(name, controlpanel, nextel) {
-    gui.prototype.datasetGUIObject[name] = undefined;
+gui.prototype.reinsertCombo = function(name, nextel) {
+    this.datasetGUIObject[name] = undefined;
     var comboDataList = document.getElementById(name + "ComboDataList");
     comboDataList.parentElement.removeChild(comboDataList);
-    gui.prototype.createCombobox(controlpanel, name, nextel);
+    this.createCombobox(name, nextel);
 }
 gui.prototype.datasetSelected = function(e) {
-    var controlpanel = document.getElementById("controlpanel");
-
     var comboDataList = e.srcElement;
     var selectedIndex = comboDataList.selectedIndex;
     var selectedText = comboDataList.children[selectedIndex].childNodes[0].data;
     if (comboDataList.id == "observatoryComboDataList") {
-        gui.prototype.datasetGUIObject.observatory = selectedText;
+        this.datasetGUIObject.observatory = selectedText;
 
-        var nextel = gui.prototype.datasetGUIObject.data[gui.prototype.datasetGUIObject.observatory].children;
-        gui.prototype.reinsertCombo("instrument", controlpanel, nextel);
+        var nextel = this.datasetGUIObject.data[this.datasetGUIObject.observatory].children;
+        this.reinsertCombo("instrument", nextel);
 
-        nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-        gui.prototype.reinsertCombo("detector", controlpanel, nextel);
+        nextel = nextel[this.datasetGUIObject.instrument].children;
+        this.reinsertCombo("detector", nextel);
 
-        nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-        gui.prototype.reinsertCombo("measurement", controlpanel, nextel);
+        nextel = nextel[this.datasetGUIObject.instrument].children;
+        this.reinsertCombo("measurement", nextel);
 
     } else if (comboDataList.id == "instrumentComboDataList") {
-        gui.prototype.datasetGUIObject.instrument = selectedText;
-        gui.prototype.datasetGUIObject.detector = undefined;
+        this.datasetGUIObject.instrument = selectedText;
+        this.datasetGUIObject.detector = undefined;
 
-        var nextel = gui.prototype.datasetGUIObject.data[gui.prototype.datasetGUIObject.observatory].children;
-        nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-        gui.prototype.reinsertCombo("detector", controlpanel, nextel);
+        var nextel = this.datasetGUIObject.data[this.datasetGUIObject.observatory].children;
+        nextel = nextel[this.datasetGUIObject.instrument].children;
+        this.reinsertCombo("detector", nextel);
 
-        nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-        gui.prototype.reinsertCombo("measurement", controlpanel, nextel);
+        nextel = nextel[this.datasetGUIObject.instrument].children;
+        this.reinsertCombo("measurement", nextel);
 
     } else if (comboDataList.id == "detectorComboDataList") {
-        gui.prototype.datasetGUIObject.detector = selectedText;
-        var nextel = gui.prototype.datasetGUIObject.data[gui.prototype.datasetGUIObject.observatory].children;
-        nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-        nextel = nextel[gui.prototype.datasetGUIObject.measurement].children;
-        gui.prototype.reinsertCombo("measurement", controlpanel, nextel);
+        this.datasetGUIObject.detector = selectedText;
+        var nextel = this.datasetGUIObject.data[this.datasetGUIObject.observatory].children;
+        nextel = nextel[this.datasetGUIObject.instrument].children;
+        nextel = nextel[this.datasetGUIObject.measurement].children;
+        this.reinsertCombo("measurement", nextel);
 
     } else if (comboDataList.id == "measurementComboDataList") {
-        gui.prototype.datasetGUIObject.detector = selectedText;
+        this.datasetGUIObject.detector = selectedText;
     }
 }
 
-gui.prototype.createCombobox = function(controlpanel, name, data) {
+gui.prototype.createCombobox = function(name, data) {
     var comboDataList = document.createElement("select");
     comboDataList.setAttribute("id", name + "ComboDataList");
     var index = 0;
     var foundindex = -1;
     for ( var key in data) {
-        if (gui.prototype.datasetGUIObject[name] === undefined) {
-            gui.prototype.datasetGUIObject[name] = key;
+        if (this.datasetGUIObject[name] === undefined) {
+            this.datasetGUIObject[name] = key;
         }
         var comboDataListOption = document.createElement("option");
         comboDataListOption.setAttribute("value", index);
         comboDataListOption.innerHTML = key;
         comboDataList.appendChild(comboDataListOption);
-        if (gui.prototype.datasetGUIObject[name] == key) {
+        if (this.datasetGUIObject[name] == key) {
             foundindex = index;
         }
         index++;
     }
-    comboDataList.onchange = gui.prototype.datasetSelected;
-    gui.prototype.datasetGUIObject[name + "HtmlElement"] = comboDataList;
+    comboDataList.onchange = this.datasetSelected;
+    this.datasetGUIObject[name + "HtmlElement"] = comboDataList;
     comboDataList.selectIndex = foundindex;
     comboDataList.value = foundindex;
-    controlpanel.appendChild(comboDataList);
+    this.controlpanel.appendChild(comboDataList);
 }
-gui.prototype.createDatebox = function(controlpanel, id, number) {
+gui.prototype.createDatebox = function(id, number) {
     var dateEl = document.createElement("input");
     dateEl.id = id;
     dateEl.setAttribute("type", "textarea");
@@ -82,42 +86,41 @@ gui.prototype.createDatebox = function(controlpanel, id, number) {
     dateEl.addEventListener("scroll", function(e) {
         console.log("EVENT" + e);
     });
-    gui.prototype.datasetGUIObject[id] = dateEl;
-    controlpanel.appendChild(dateEl);
+    this.datasetGUIObject[id] = dateEl;
+    this.controlpanel.appendChild(dateEl);
 }
 
 gui.prototype.buildUrl = function() {
-    var url = gui.prototype.datasetGUIObject.baseurl;
+    var url = this.datasetGUIObject.baseurl;
     url += "&observatory=";
-    var obs = gui.prototype.datasetGUIObject["observatoryHtmlElement"];
+    var obs = this.datasetGUIObject["observatoryHtmlElement"];
     url += obs.children[obs.selectedIndex].childNodes[0].data;
     url += "&instrument=";
-    var obs = gui.prototype.datasetGUIObject["instrumentHtmlElement"];
+    var obs = this.datasetGUIObject["instrumentHtmlElement"];
     url += obs.children[obs.selectedIndex].childNodes[0].data;
     url += "&detector=";
-    var det = gui.prototype.datasetGUIObject["detectorHtmlElement"]
+    var det = this.datasetGUIObject["detectorHtmlElement"]
     url += det.children[det.selectedIndex].childNodes[0].data;
     url += "&measurement=";
-    var meas = gui.prototype.datasetGUIObject["measurementHtmlElement"]
+    var meas = this.datasetGUIObject["measurementHtmlElement"]
     url += meas.children[meas.selectedIndex].childNodes[0].data;
     url += "&startTime=";
-    url += gui.prototype.datasetGUIObject["startTime"].value + "Z";
+    url += this.datasetGUIObject["startTime"].value + "Z";
     url += "&endTime=";
-    url += gui.prototype.datasetGUIObject["endTime"].value + "Z";
+    url += this.datasetGUIObject["endTime"].value + "Z";
     url += "&cadence=1800&jpip=true&verbose=true&linked=true";
     return url;
 }
 
 gui.prototype.setBeginAndEndDate = function() {
-    beginDate = parseDate(gui.prototype.datasetGUIObject["startTime"].value, 0);
-    endDate = parseDate(gui.prototype.datasetGUIObject["endTime"].value, 0);
+    beginDate = parseDate(this.datasetGUIObject["startTime"].value, 0);
+    endDate = parseDate(this.datasetGUIObject["endTime"].value, 0);
     currentDate = beginDate;
 }
 
 gui.prototype.initGui = function(data) {
-    gui.prototype.datasetGUIObject.data = data;
-    gui.prototype.datasetGUIObject.baseurl = "http://swhv.oma.be/hv/api/index.php?action=getJPX";
-    var local_controlpanel = document.getElementById("local_controlpanel");
+    this.datasetGUIObject.data = data;
+    this.datasetGUIObject.baseurl = "http://swhv.oma.be/hv/api/index.php?action=getJPX";
 
     var serverLabel = document.createElement("label");
     serverLabel.setAttribute("for", "serverExternal");
@@ -129,35 +132,35 @@ gui.prototype.initGui = function(data) {
     serverInput.setAttribute("type", "text");
     serverInput.setAttribute("size", "40");
     serverInput.setAttribute("value", "http://127.0.0.1:8090/");
-    local_controlpanel.appendChild(serverInput);
-    local_controlpanel.appendChild(document.createElement("br"));
+    this.local_controlpanel.appendChild(serverInput);
+    this.local_controlpanel.appendChild(document.createElement("br"));
 
     var imageInputLabel = document.createElement("label");
     imageInputLabel.setAttribute("for", "imageExternal");
     imageInputLabel.innerHTML = "JPX: ";
-    local_controlpanel.appendChild(imageInputLabel);
+    this.local_controlpanel.appendChild(imageInputLabel);
     var imageInput = document.createElement("input");
     imageInput.id = "imageExternal";
     imageInput.setAttribute("type", "text");
     imageInput.setAttribute("size", "120");
     imageInput.setAttribute("value", "SWAP.jpx");
-    local_controlpanel.appendChild(imageInput);
-    local_controlpanel.appendChild(document.createElement("br"));
+    this.local_controlpanel.appendChild(imageInput);
+    this.local_controlpanel.appendChild(document.createElement("br"));
 
     var numberOfFramesLabel = document.createElement("label");
     numberOfFramesLabel.setAttribute("for", "imageExternal");
     numberOfFramesLabel.innerHTML = "Max number of frames: ";
-    local_controlpanel.appendChild(numberOfFramesLabel);
+    this.local_controlpanel.appendChild(numberOfFramesLabel);
     var numberOfFramesInput = document.createElement("input");
     numberOfFramesInput.id = "numberOfFrames";
     numberOfFramesInput.setAttribute("type", "text");
     numberOfFramesInput.setAttribute("value", "48");
-    local_controlpanel.appendChild(numberOfFramesInput);
-    local_controlpanel.appendChild(document.createElement("br"));
+    this.local_controlpanel.appendChild(numberOfFramesInput);
+    this.local_controlpanel.appendChild(document.createElement("br"));
 
     var externalbutton = document.createElement("button");
     externalbutton.innerHTML = "Load external";
-    local_controlpanel.appendChild(externalbutton);
+    this.local_controlpanel.appendChild(externalbutton);
     externalbutton.onclick = function() {
         var serverName = document.getElementById("serverExternal").value;
         var imageName = document.getElementById("imageExternal").value;
@@ -166,44 +169,48 @@ gui.prototype.initGui = function(data) {
         objectList.push(new solarJPIP(serverName, imageName, frameCount, 512));
     }
 
-    var controlpanel = document.getElementById("controlpanel");
+    var loadButton = document.createElement("button");
+    loadButton.innerHTML = "Load";
+    this.controlpanel.appendChild(loadButton);
+    loadButton.addEventListener("click", this, false);
+    loadButton.setAttribute("data-type", "loadButton");
 
-    var button = document.createElement("button");
-    button.innerHTML = "Load";
-    controlpanel.appendChild(button);
-    button.onclick = function() {
-        var success = function(data) {
-            var jpxfile = data.uri;
-            var jpxparts = jpxfile.split("movies");
-            objectList.push(new solarJPIP("http" + jpxparts[0].substring(4, jpxparts[0].length), "movies" + jpxparts[1], data.frames.length, 512));
-            // objectList.push(new solarJPIP("http://localhost:8090/", "movies"
-            // + jpxparts[1], data.frames.length, 512));
-        };
-        getJSON(gui.prototype.buildUrl(), success, function(e) {
-            console.log();
-        });
-        gui.prototype.setBeginAndEndDate();
-    }
-    controlpanel.appendChild(document.createElement("br"));
+    this.controlpanel.appendChild(document.createElement("br"));
 
     var dateNumber = 1;
-    gui.prototype.createDatebox(controlpanel, "startTime", dateNumber);
-    controlpanel.appendChild(document.createElement("br"));
+    this.createDatebox("startTime", dateNumber);
+    this.controlpanel.appendChild(document.createElement("br"));
     dateNumber--;
-    gui.prototype.createDatebox(controlpanel, "endTime", dateNumber);
-    controlpanel.appendChild(document.createElement("br"));
-    var nextel = gui.prototype.datasetGUIObject.data;
-    gui.prototype.datasetGUIObject["observatory"] = "SDO";
-    gui.prototype.createCombobox(controlpanel, "observatory", nextel);
-    nextel = nextel[gui.prototype.datasetGUIObject.observatory].children;
-    gui.prototype.createCombobox(controlpanel, "instrument", gui.prototype.datasetGUIObject.data[gui.prototype.datasetGUIObject.observatory].children);
-    nextel = nextel[gui.prototype.datasetGUIObject.instrument].children;
-    gui.prototype.createCombobox(controlpanel, "detector", nextel);
-    nextel = nextel[gui.prototype.datasetGUIObject.detector].children;
-    gui.prototype.createCombobox(controlpanel, "measurement", nextel);
-    gui.prototype.createVideoBar();
+    this.createDatebox("endTime", dateNumber);
+    this.controlpanel.appendChild(document.createElement("br"));
+    var nextel = this.datasetGUIObject.data;
+    this.datasetGUIObject["observatory"] = "SDO";
+    this.createCombobox("observatory", nextel);
+    nextel = nextel[this.datasetGUIObject.observatory].children;
+    this.createCombobox("instrument", this.datasetGUIObject.data[this.datasetGUIObject.observatory].children);
+    nextel = nextel[this.datasetGUIObject.instrument].children;
+    this.createCombobox("detector", nextel);
+    nextel = nextel[this.datasetGUIObject.detector].children;
+    this.createCombobox("measurement", nextel);
+    this.createVideoBar();
 };
 
+gui.prototype.handleEvent = function(e) {
+    switch (e.type) {
+        case "click":
+            var elementType = e.srcElement.attributes["data-type"].value;
+            if (elementType == "loadButton") {
+                var success = function(data) {
+                    var jpxfile = data.uri;
+                    var jpxparts = jpxfile.split("movies");
+                    objectList.push(new solarJPIP("http" + jpxparts[0].substring(4, jpxparts[0].length), "movies" + jpxparts[1], data.frames.length, 512));
+                };
+                getJSON(this.buildUrl(), success, function(e) {
+                });
+                this.setBeginAndEndDate();
+            }
+    }
+}
 gui.prototype.createVideoBar = function() {
     var videoButton = document.getElementById("videoPlayButton");
     videoButton.addEventListener("click", function() {
